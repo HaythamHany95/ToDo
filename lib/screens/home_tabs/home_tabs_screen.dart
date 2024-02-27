f50d3697d957b1c8becf3ba795d5408df48b1f16
HaythamHany95 <haythamhany95@gmail.com>
2024-02-27 21:30:52 +0200

    +Splash Screen Added

---
+++ lib/screens/home_tabs/home_tabs_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class HomeTabsScreen extends StatelessWidget {
+  static const String routeName = "home_tabs_screen";
+
+  const HomeTabsScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
---
+++ lib/utilities/my_theme.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+/// light mode, dark mode,
+class MyTheme {
+  static Color primaryColor = const Color(0xFF5D9CEC);
+  static Color lightBackgroundColor = const Color(0xFFDFECDB);
+  static Color darkBackgroundColor = const Color(0xFF060E1E);
+  static Color greyLightColor = const Color(0xFFA9A9A9);
+  static Color greyDarkColor = const Color(0xFF707070);
+  static Color whiteColor = const Color(0xFFFFFFFF);
+  static Color blackColor = const Color(0xFF363636);
+  static Color petrolColor = const Color(0xFF141922);
+  static Color greenColor = const Color(0xFF61E757);
+  static Color redColor = const Color(0xFFEC4B4B);
+  static ThemeData lightMode = ThemeData();
+  static ThemeData darkMode = ThemeData();
+}
