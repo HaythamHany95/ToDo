5b09084180da116607d34b88c3da8f0c3f77482e
HaythamHany95 <haythamhany95@gmail.com>
2024-03-02 14:46:27 +0200

    + Firebase Integrated && lowering `flutter_native_splash` to 2.3.10

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:to_do_app/screens/home_tabs/home_tabs_screen.dart';
 import 'package:to_do_app/utilities/my_theme.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
